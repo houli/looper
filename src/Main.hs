3d5674f3bd4851e403b83fb38ac3a954d1fdb5f5
Eoin Houlihan <ehoulih@tcd.ie>
2016-12-22 21:41:58 +0000

    Add unused variable checker

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,11 @@
 module Main where
 
+import           Data.List (intercalate)
 import qualified Data.Map as Map
 
 import Eval
 import Program
+import StaticAnalysis
 
 testExpr :: Expr
 testExpr = Const (I 3)
@@ -11,8 +13,23 @@ testExpr = Const (I 3)
 testProgram :: Program
 testProgram = Assign "a" (Const (I 3)) :> Print (Var "a")
 
+test2 :: Program
+test2 = Assign "unused" (Const (I 3)) :>
+        Assign "arg" (Const (I 10)) :>
+        Assign "scratch" (Var "arg") :>
+        Assign "total" (Const (I 1)) :>
+        While (Gt (Var "scratch") (Const (I 1))) (
+           Assign "total" (Mul (Var "total") (Var "scratch")) :>
+           Assign "scratch" (Sub (Var "scratch") (Const (I 1))) :>
+           Print (Var "scratch")
+        ) :>
+        Print (Var "total")
+
 main :: IO ()
 main = do
+  case unusedVariables test2 of
+    [] -> pure ()
+    vars -> putStrLn $ "Unused variables: " ++ intercalate ", " vars
   run testProgram
   case runEval Map.empty (eval testExpr) of
     Left err -> print err
---
+++ src/StaticAnalysis.hs
@@ -0,0 +1,44 @@
+module StaticAnalysis
+  ( unusedVariables
+  ) where
+
+import Data.List (nub, (\\))
+
+import Eval
+import Program
+
+unusedVariables :: Program -> [String]
+unusedVariables p = defines p \\ uses p
+
+defines :: Program -> [String]
+defines = nub . go
+  where go (Assign s _) = [s]
+        go (If _ p0 p1) = go p0 ++ go p1
+        go (While _ p) = go p
+        go (Print _) = []
+        go (p0 :> p1) = go p0 ++ go p1
+        go (Try p0 p1) = go p0 ++ go p1
+
+uses :: Program -> [String]
+uses = nub . go
+  where go (Assign _ e) = usesExpr e
+        go (If cond p0 p1) = usesExpr cond ++ uses p0 ++ uses p1
+        go (While cond p) = usesExpr cond ++ uses p
+        go (Print e) = usesExpr e
+        go (p0 :> p1) = uses p0 ++ uses p1
+        go (Try p0 p1) = go p0 ++ go p1
+
+usesExpr :: Expr -> [String]
+usesExpr = nub . go
+  where go (Const _) = []
+        go (Add e0 e1) = usesExpr e0 ++ usesExpr e1
+        go (Sub e0 e1) = usesExpr e0 ++ usesExpr e1
+        go (Mul e0 e1) = usesExpr e0 ++ usesExpr e1
+        go (Div e0 e1) = usesExpr e0 ++ usesExpr e1
+        go (And e0 e1) = usesExpr e0 ++ usesExpr e1
+        go (Or e0 e1) = usesExpr e0 ++ usesExpr e1
+        go (Not e) = usesExpr e
+        go (Eq e0 e1) = usesExpr e0 ++ usesExpr e1
+        go (Gt e0 e1) = usesExpr e0 ++ usesExpr e1
+        go (Lt e0 e1) = usesExpr e0 ++ usesExpr e1
+        go (Var s) = [s]
