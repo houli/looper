fcd54233b1c94c1610261b4143c535525b162667
Eoin Houlihan <ehoulih@tcd.ie>
2016-12-29 15:51:51 +0000

    Clean up main

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,10 @@
 module Main where
 
+import Control.Monad (unless)
+
 import Interpreter
 import Program
 import StaticAnalysis
-
-testProgram :: Program
-testProgram = Assign "a" (Const (I 3)) :> Print (Var "a")
-
-testIf :: Program
-testIf = Assign "i" (Const (I 3)) :>
-         If (Gt (Var "i") (Const (I 2))) (
-           Assign "i" (Sub (Var "i") (Const (I 1))) :>
-           Print (Var "i")
-         ) (
-           Assign "i" (Const (I 10)) :>
-           Print (Var "i")
-         ) :>
-         Assign "i" (Const (I 20)) :>
-         Print (Var "i")
 
 test2 :: Program
 test2 = Assign "unused" (Const (I 3)) :>
@@ -33,8 +20,7 @@ test2 = Assign "unused" (Const (I 3)) :>
 
 main :: IO ()
 main = do
-  case unusedVariables test2 of
-    [] -> pure ()
-    vars -> mapM_ printUnused vars
+  let vars = unusedVariables test2
+  unless (null vars) (mapM_ printUnused vars)
   run test2
     where printUnused var = putStrLn $ show var ++ " is assigned but not used"
